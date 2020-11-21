b27f9ff97e066fbbfb8590398919d198a6e5bdd2
unknown <acirlomaneanu@stud.acs.upb.ro>
2020-11-21 11:37:44 +0200

    Tema

---
+++ src/actor/Awards.java
@@ -0,0 +1,17 @@
+package actor;
+
+public class Awards {
+    private String awardType;
+    private int awardNums;
+
+
+
+    public String getAwardType() {  return awardType;    }
+
+    public int getAwardNums() { return awardNums; }
+
+
+    public void setAwardType(String awardType) { this.awardType = awardType; }
+
+    public void setAwardNums(int awardNums) { this.awardNums = awardNums; }
+}
